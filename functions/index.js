1a14b057df5d13d6947f85407184a66818e5b341
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-02-20 15:07:11 +0530

    Initial pages
    Co-authored-by: Abhishek Sai <34051505+Abhishek-Sai@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'),
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    admin = require('firebase-admin');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+var db = admin.firestore();
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
+
+exports.app = functions.https.onRequest(app);
+
+
