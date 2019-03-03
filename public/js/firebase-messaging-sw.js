b7b62a82fee14a77407e10bfb6693f38834295c3
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-03-03 12:40:50 +0530

    FCM

---
+++ public/js/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.8.4/firebase-messaging.js');
+firebase.initializeApp({
+	messagingSenderId: '254746284439'
+});
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+	console.log(
+		'[firebase-messaging-sw.js] Received background message ',
+		payload
+	);
+	const notificationTitle = 'Equity Sports';
+	const notificationOptions = {
+		body: 'Background Message body.',
+		icon: '/android-chrome-192x192.png'
+	};
+
+	return self.registration.showNotification(
+		notificationTitle,
+		notificationOptions
+	);
+});
